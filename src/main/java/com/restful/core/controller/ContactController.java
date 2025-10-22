b8ea2f757cb9204972191f6a593f388d68494e51
AriqHakim <ariq.hakim2002@gmail.com>
2025-10-22 08:09:30 +0700

    fix bug on contact path URL

--- src/main/java/com/restful/core/controller/ContactController.java
+++ src/main/java/com/restful/core/controller/ContactController.java
@@ -26,67 +26,67 @@ import com.restful.core.entity.User;
 
 @RestController
 public class ContactController {
-    @Autowired
-    private ContactServices contactServices;
+        @Autowired
+        private ContactServices contactServices;
 
-    @PostMapping(path = "/api/contacts", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public WebResponse<ContactResponse> create(User user, @RequestBody CreateContactRequest request) {
-        ContactResponse contactResponse = contactServices.create(user, request);
-        return WebResponse.<ContactResponse>builder()
-                .data(contactResponse)
-                .build();
-    }
+        @PostMapping(path = "/api/contacts", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+        public WebResponse<ContactResponse> create(User user, @RequestBody CreateContactRequest request) {
+                ContactResponse contactResponse = contactServices.create(user, request);
+                return WebResponse.<ContactResponse>builder()
+                                .data(contactResponse)
+                                .build();
+        }
 
-    @GetMapping(path = "/api/contacts/{contactId}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public WebResponse<ContactResponse> get(User user, @PathVariable("contactId") String contactId) {
-        ContactResponse contactResponse = contactServices.get(user, contactId);
-        return WebResponse.<ContactResponse>builder()
-                .data(contactResponse)
-                .build();
-    }
+        @GetMapping(path = "/api/contacts/{contactId}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+        public WebResponse<ContactResponse> get(User user, @PathVariable("contactId") String contactId) {
+                ContactResponse contactResponse = contactServices.get(user, contactId);
+                return WebResponse.<ContactResponse>builder()
+                                .data(contactResponse)
+                                .build();
+        }
 
-    @PutMapping(path = "/api/contacts/{contactId", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public WebResponse<ContactResponse> update(User user,
-            @RequestBody UpdateContactRequest request,
-            @PathVariable("contactId") String contactId) {
-        request.setId(contactId);
-        ContactResponse contactResponse = contactServices.udpate(user, request);
-        return WebResponse.<ContactResponse>builder()
-                .data(contactResponse)
-                .build();
-    }
+        @PutMapping(path = "/api/contacts/{contactId}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+        public WebResponse<ContactResponse> update(User user,
+                        @RequestBody UpdateContactRequest request,
+                        @PathVariable("contactId") String contactId) {
+                request.setId(contactId);
+                ContactResponse contactResponse = contactServices.udpate(user, request);
+                return WebResponse.<ContactResponse>builder()
+                                .data(contactResponse)
+                                .build();
+        }
 
-    @DeleteMapping(path = "/api/contacts/{contactId}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public WebResponse<String> delete(User user, @PathVariable("contactId") String contactId) {
-        contactServices.delete(user, contactId);
-        return WebResponse.<String>builder()
-                .data("OK")
-                .build();
-    }
+        @DeleteMapping(path = "/api/contacts/{contactId}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+        public WebResponse<String> delete(User user, @PathVariable("contactId") String contactId) {
+                contactServices.delete(user, contactId);
+                return WebResponse.<String>builder()
+                                .data("OK")
+                                .build();
+        }
 
-    @GetMapping(path = "/api/contacts", produces = MediaType.APPLICATION_JSON_VALUE)
-    public WebResponse<List<ContactResponse>> search(User user,
-            @RequestParam(value = "name", required = false) String name,
-            @RequestParam(value = "email", required = false) String email,
-            @RequestParam(value = "phone", required = false) String phone,
-            @RequestParam(value = "page", required = true, defaultValue = "0") Integer page,
-            @RequestParam(value = "size", required = true, defaultValue = "10") Integer size) {
-        SearchContactRequest request = SearchContactRequest.builder()
-                .page(page)
-                .size(size)
-                .name(name)
-                .phone(phone)
-                .email(email)
-                .build();
+        @GetMapping(path = "/api/contacts", produces = MediaType.APPLICATION_JSON_VALUE)
+        public WebResponse<List<ContactResponse>> search(User user,
+                        @RequestParam(value = "name", required = false) String name,
+                        @RequestParam(value = "email", required = false) String email,
+                        @RequestParam(value = "phone", required = false) String phone,
+                        @RequestParam(value = "page", required = true, defaultValue = "0") Integer page,
+                        @RequestParam(value = "size", required = true, defaultValue = "10") Integer size) {
+                SearchContactRequest request = SearchContactRequest.builder()
+                                .page(page)
+                                .size(size)
+                                .name(name)
+                                .phone(phone)
+                                .email(email)
+                                .build();
 
-        Page<ContactResponse> responses = contactServices.search(user, request);
-        return WebResponse.<List<ContactResponse>>builder()
-                .data(responses.getContent())
-                .paging(PagingResponse.builder()
-                        .currentPage(responses.getNumber())
-                        .totalPage(responses.getTotalPages())
-                        .size(responses.getSize())
-                        .build())
-                .build();
-    }
+                Page<ContactResponse> responses = contactServices.search(user, request);
+                return WebResponse.<List<ContactResponse>>builder()
+                                .data(responses.getContent())
+                                .paging(PagingResponse.builder()
+                                                .currentPage(responses.getNumber())
+                                                .totalPage(responses.getTotalPages())
+                                                .size(responses.getSize())
+                                                .build())
+                                .build();
+        }
 }
